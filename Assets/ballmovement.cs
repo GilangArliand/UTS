c66f0bf8f75cda8811510a65ffef47a081341544
GilangArliand <18523205@students.uii.ac.id>
2020-04-15 17:51:59 +0700

    GamePong

--- Assets/ballmovement.cs
+++ Assets/ballmovement.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class ballmovement : MonoBehaviour
 {
-    public int speed = 30;
+    //public int speed = 30;
     // public int counter = 0;
     public int counterP1 = 0;
     public int counterP2 = 0;
@@ -17,8 +17,11 @@ public class ballmovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     { 
-        // scoreText.text = counter.ToString();
-        sesuatu.velocity = new Vector2 (-1,-1)*speed;
+        int x = Random.Range(0, 2) * 2 - 1;
+        int y = Random.Range(0, 2) * 2 - 1;
+        int speed = Random.Range(20, 26);
+        sesuatu.velocity = new Vector2 (x, y) * speed;
+        sesuatu.GetComponent<Transform>().position = Vector2.zero;
         animtr.SetBool("IsMove",true);
     }
 
@@ -43,25 +46,35 @@ public class ballmovement : MonoBehaviour
 
     // perintah
     IEnumerator jedabolaP1() {
+        
+        int x = Random.Range(0, 2) * 2 - 1;
+        int y = Random.Range(0, 2) * 2 - 1;
+        int speed = Random.Range(20, 26);
+
     sesuatu.velocity = Vector2.zero;
     animtr.SetBool("IsMove",false);
     sesuatu.GetComponent<Transform>().position = Vector2.zero;
     counterP1++;
     scoreText1.text = counterP1.ToString();
     yield return new WaitForSeconds(1);
-    sesuatu.velocity = new Vector2 (-1,-1)*speed;//Jika P1 mencetak score maka bola dimulai ke arah P1
+    sesuatu.velocity = new Vector2 (x, y)*speed;//Jika P1 mencetak score maka bola dimulai ke arah P1
     animtr.SetBool("IsMove",true);
     
     }
 
     IEnumerator jedabolaP2() {
+        
+        int x = Random.Range(0, 2) * 2 - 1;
+        int y = Random.Range(0, 2) * 2 - 1;
+        int speed = Random.Range(20, 26);
+
     sesuatu.velocity = Vector2.zero;
     animtr.SetBool("IsMove",false);
     sesuatu.GetComponent<Transform>().position = Vector2.zero;
     counterP2++;
     scoreText2.text = counterP2.ToString();
     yield return new WaitForSeconds(1);
-    sesuatu.velocity = new Vector2 (1,1)*speed; //Jika P2 mencetak score maka bola dimulai ke arah P2
+    sesuatu.velocity = new Vector2 (x, y) * speed; //Jika P2 mencetak score maka bola dimulai ke arah P2
     animtr.SetBool("IsMove",true);
     
     }
